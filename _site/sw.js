a0679134acb7578dc2a4421be004ca6fc1b1b6e4
Jeong Hun Nam <njh3421@gmail.com>
2024-02-06 11:19:48 +0900

    change local

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707122143';
+const cacheName = 'chirpy-1707185855';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
